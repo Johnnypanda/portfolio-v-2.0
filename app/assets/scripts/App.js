c56b824e09dc7f3127c5e04fceafe9ace27febe2
Artem <afilinov93@gmail.com>
2018-02-27 20:30:06 +0100

    Completed reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,8 @@
 import fontawesome from '@fortawesome/fontawesome';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".skills-item"), "85%");
+new RevealOnScroll($(".works-item"), "60%");
