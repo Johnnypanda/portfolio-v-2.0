1e48cf1647f69f83a945a27395e9e5b5ecb1eab1
Artem <afilinov93@gmail.com>
2018-02-27 18:14:59 +0100

    Completed basic styling for the nav section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,4 @@
-import fontawesome from '@fortawesome/fontawesome'
+import fontawesome from '@fortawesome/fontawesome';
+import MobileMenu from './modules/MobileMenu';
+
+var mobileMenu = new MobileMenu();
