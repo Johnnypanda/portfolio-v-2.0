8dae9e926d6a3a15739a193fc2a0c72492defd66
Artem <afilinov93@gmail.com>
2018-02-28 15:55:37 +0100

    Completed the header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,11 @@
+import $ from 'jquery';
 import fontawesome from '@fortawesome/fontawesome';
 import MobileMenu from './modules/MobileMenu';
+import StickyHeader from './modules/StickyHeader';
 import RevealOnScroll from './modules/RevealOnScroll';
-import $ from 'jquery';
+
 
 var mobileMenu = new MobileMenu();
+var stickyHeader = new StickyHeader();
 new RevealOnScroll($(".skills-item"), "85%");
 new RevealOnScroll($(".works-item"), "60%");
