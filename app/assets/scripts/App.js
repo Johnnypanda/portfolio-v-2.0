37730b127bdb0ea3c827bda7b018c2e636f10577
Artem <afilinov93@gmail.com>
2018-02-28 21:41:38 +0100

    Completed modal window (almost, to be honest)

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,9 +3,10 @@ import fontawesome from '@fortawesome/fontawesome';
 import MobileMenu from './modules/MobileMenu';
 import StickyHeader from './modules/StickyHeader';
 import RevealOnScroll from './modules/RevealOnScroll';
-
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 var stickyHeader = new StickyHeader();
 new RevealOnScroll($(".skills-item"), "85%");
 new RevealOnScroll($(".works-item"), "60%");
+var modal = new Modal();
